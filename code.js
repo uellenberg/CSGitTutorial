d58374b19b02c95740f230dfebc235f3a76c1709
Micah Hogan <micahhogan@pop-os.localdomain>
2022-10-19 16:33:20 -0700

    micah's commit

--- code.js
+++ code.js
@@ -2,11 +2,9 @@ console.log("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 console.log("Mwahahah");
 console.log("Bill loves Microsoft");
 console.log("Travis is an expert programmer.");
-<<<<<<< HEAD
 console.log("Travis is an expert programmer.");
 console.log("Koolaid Man says \"OH YEAHHHH!\"");
 console.log("Never gonna give you up...");
-=======
 console.log("I want to create another merge conflict!");
 console.log("Please work!");
->>>>>>> 5b378ac02bf029fe647fa2941b1f0e1f3dd3eaf7
+console.log("Successful merge conflict commited and pushed!");
